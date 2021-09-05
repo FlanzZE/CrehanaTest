b6165360c4491ae571ffa13ca3b801b1421792a3
Victor Mexicano <vmmexicano@gmail.com>
2021-09-04 21:19:11 -0500

    feat: chakra-ui provider

--- pages/tickets/index.jsx
+++ pages/tickets/index.jsx
@@ -4,19 +4,24 @@ export const Index = () => {
   const [tickets, settickets] = useState("25,25,50");
   const hasChange = bills => {
     const arrayBill = bills.split(",")
-    console.log('==================arr∫==================');
-    console.log(arrayBill);
-    console.log('====================================');
-    let [bill25, bill50] = [0, 0];
+    let bill25 = 0;
+    let bill50 = 0;
     arrayBill.map(el => {
-      console.log("bill", bill25, bill50)
-      parseInt(el) == 25 ? bill25++ : parseInt(el) == 50 ? (bill50++, bill25--) : (bill50 ? bill50-- : bill25 -= 2, bill25--);
-      if (bill25 <= 0) return `NO`;
+      parseInt(el) == 25
+        ? bill25++
+        : parseInt(el) == 50
+          ? (bill50++, bill25--)
+          : (
+            bill50
+              ? bill50--
+              : bill25 -= 2, bill25--
+          );
+      if (bill25 < 0) return false;
     })
-    if (bill25 > 0) {
-      return `YES`;
+    if (bill25 >= 0) {
+      return true;
     }
-    return "NO"
+    return false
   };
   return (
     <div style={{ padding: "1rem" }}>
@@ -35,7 +40,7 @@ export const Index = () => {
             settickets(value.replace(/\s/g, ''))
           }
         } />
-      <h2> {hasChange(tickets)} </h2>
+      <h2> {hasChange(tickets) ? "Aun " : "No"} hay cambio </h2>
     </div>
   )
 }
